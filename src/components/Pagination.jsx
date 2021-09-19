1fb3b95056f737f539426988faea56ec20a23df6
Jakob <jsandberg_8@hotmail.com>
2021-09-19 19:46:50 +0200

    jaja

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -5,14 +5,16 @@ const Pagination = ({ page, setPage, isPreviousData, total }) => {
   return (
     <div className="pagination d-flex justify-content-between align-items-center m-4">
       <Button
+        variant="danger"
         onClick={() => setPage((currentPage) => Math.max(currentPage - 1, 1))}
         disabled={page === 1}
       >
         Previous Page
       </Button>
-      <span>Current Page: {page}</span>
+      <span>Page: {page}</span>
 
       <Button
+        variant="danger"
         onClick={() => {
           if (!isPreviousData && page !== total) {
             setPage((currentPage) => currentPage + 1);
